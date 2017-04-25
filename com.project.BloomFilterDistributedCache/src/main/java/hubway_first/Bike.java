db1130bb840c41a77f3ac98aefbed9702c0c0cb2
neshant <neshantsharma89@gmail.com>
2017-04-24 22:14:04 -0400

    Bloomfilter with distributed cache

---
+++ com.project.BloomFilterDistributedCache/src/main/java/hubway_first/Bike.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hubway_first;
+
+/**
+ *
+ * @author bhavesh
+ */
+public class Bike {
+	
+	final String userType;
+	final String gender;
+
+
+	Bike(String userType, String gender) {
+
+		this.userType = userType;
+		this.gender = gender;
+		
+	}
+}
